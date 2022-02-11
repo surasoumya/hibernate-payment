4c235b636e9f0c4a04e26be22533852e62cb2afb
surasoumya <98646568+surasoumya@users.noreply.github.com>
2022-02-11 15:24:43 +0530

    Add files via upload

--- CreditCard.java
+++ CreditCard.java
@@ -1,8 +1,22 @@
 package com.mondee;
 
-public class CreditCard extends Payment {
-	private String creditCardType;
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
+@Entity  
+@Table(name="CC2")  
+@AttributeOverrides({  
+    @AttributeOverride(name="paymentId", column=@Column(name="CCpid")),  
+    @AttributeOverride(name="amount", column=@Column(name="CCamount"))  
+})  
+public class CreditCard extends Payment {
+
+	@Column(name = "CCType")  
+	private String creditCardType;
+	
 	public String getCreditCardType() {
 		return creditCardType;
 	}
@@ -11,5 +25,5 @@ public class CreditCard extends Payment {
 		this.creditCardType = creditCardType;
 	}
 	
-	
+
 }
